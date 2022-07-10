b04c4e6f310fd3651bd374c2f1824c87da365e4b
MOhammad reza <mohammadreza@MOhammads-MacBook-Pro.local>
2022-07-10 20:25:21 +0430

    some things updated

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Expenses from "./components/Expenses";
 import NewExpenses from "./components/NewExpense/NewExpenses";
 
 const BASEDEXPENESE = [
-  { id: "e1", title: "car ins", amount: 296.64, date: new Date(2021, 2, 27),},
+  { id: "e1", title: "car ins", amount: 296.64, date: new Date(2021, 2, 27) },
   { id: "e2", title: "coffee", amount: 2.99, date: new Date(2022, 9, 10) },
   { id: "e3", title: "new mug", amount: 10, date: new Date(2020, 2, 5) },
   { id: "e4", title: "new sneakers", amount: 400, date: new Date(2018, 7, 11) },
-  { id: "e5", title: "lunch", amount: 3, date: new Date(2019, 11, 29) },
+  { id: "e5", title: "lunch", amount: 3, date: new Date(2020, 11, 29) },
 ];
 // let expenseCount = BASEDEXPENESE.length ;
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -8,21 +8,28 @@ function Expenses(props) {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  let expenseContent = <p>no expense found</p>;
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onFilterChange={filterChangeHandler}
       />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          key = {expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expenseContent}
     </div>
   );
 }
